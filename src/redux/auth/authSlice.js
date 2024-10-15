d37458512a70d842141b99a0bc1da227f808c5ba
lotickles <148078790+lotickles@users.noreply.github.com>
2024-10-15 23:15:20 +0800

    repairing the login

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,82 +1,152 @@
-import { createSlice, isAnyOf } from '@reduxjs/toolkit';
-import {
-  loginThunk,
-  logoutThunk,
-  refreshThunk,
-  registerThunk,
-} from './operations';
+import { createSlice } from "@reduxjs/toolkit";
+import { register, logIn, logOut, refreshUser } from "./authOperation";
+// import { axios } from 'axios';
 
-const initialState = {
-  accessToken: '',
-  refreshToken: '',
-  sid: '',
+const initialAuthState = {
+  user: { name: null, email: null },
+  token: null,
   isLoggedIn: false,
   isRefreshing: false,
-  error: null,
   isLoading: false,
-  sessionError: false,
+  isError: false,
 };
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  extraReducers: builder =>
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialAuthState,
+  reducers: {},
+  extraReducers: (builder) => {
     builder
-      .addCase(registerThunk.fulfilled, (state, { payload }) => {
-        state.accessToken = payload.accessToken;
-        state.refreshToken = payload.refreshToken;
-        state.sid = payload.sid;
-        state.error = null;
+      .addCase(register.pending, (state, _action) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
         state.isLoggedIn = true;
-        state.sessionError = false;
       })
-      .addCase(loginThunk.fulfilled, (state, { payload }) => {
-        state.accessToken = payload.accessToken;
-        state.refreshToken = payload.refreshToken;
-        state.sid = payload.sid;
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+      })
+      .addCase(logIn.pending, (state, _action) => {
+        state.isLoading = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
         state.isLoggedIn = true;
-        state.error = null;
-        state.sessionError = false;
       })
-      .addCase(logoutThunk.fulfilled, state => {
-        return (state = initialState);
+      .addCase(logIn.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
       })
-      .addCase(refreshThunk.pending, state => {
-        state.sessionError = false;
+      .addCase(logOut.pending, (state, _action) => {
+        state.isLoading = true;
+      })
+      .addCase(logOut.fulfilled, (state, action) => {
+        state.user = { name: null, email: null };
+        state.token = null;
+        state.isLoggedIn = false;
+      })
+      .addCase(logOut.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+      })
+
+      .addCase(refreshUser.pending, (state) => {
         state.isRefreshing = true;
       })
-      .addCase(refreshThunk.fulfilled, (state, { payload }) => {
-        state.sessionError = false;
-        state.accessToken = payload.accessToken;
-        state.refreshToken = payload.refreshToken;
-        state.sid = payload.sid;
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
       })
-      .addCase(refreshThunk.rejected, state => {
-        state.sessionError = true;
+      .addCase(refreshUser.rejected, (state, action) => {
         state.isRefreshing = false;
-      })
-      .addMatcher(
-        isAnyOf(registerThunk.pending, loginThunk.pending, logoutThunk.pending),
-        (state, { payload }) => {
-          state.sessionError = false;
-          state.isLoading = true;
-          state.error = null;
-        }
-      )
-      .addMatcher(
-        isAnyOf(
-          registerThunk.rejected,
-          loginThunk.rejected,
-          logoutThunk.rejected
-        ),
-        (state, { payload }) => {
-          state.sessionError = false;
-          state.isLoading = false;
-          state.error = payload.error;
-        }
-      ),
+        state.isError = true;
+        state.isLoggedIn = false;
+      });
+  },
 });
+// import { createSlice, isAnyOf } from '@reduxjs/toolkit';
+// import {
+//   loginThunk,
+//   logoutThunk,
+//   refreshThunk,
+//   registerThunk,
+// } from './operations';
 
-export const authReducer = authSlice.reducer;
+// const initialState = {
+//   accessToken: '',
+//   refreshToken: '',
+//   sid: '',
+//   isLoggedIn: false,
+//   isRefreshing: false,
+//   error: null,
+//   isLoading: false,
+//   sessionError: false,
+// };
+
+// const authSlice = createSlice({
+//   name: 'auth',
+//   initialState,
+//   extraReducers: builder =>
+//     builder
+//       .addCase(registerThunk.fulfilled, (state, { payload }) => {
+//         state.accessToken = payload.accessToken;
+//         state.refreshToken = payload.refreshToken;
+//         state.sid = payload.sid;
+//         state.error = null;
+//         state.isLoggedIn = true;
+//         state.sessionError = false;
+//       })
+//       .addCase(loginThunk.fulfilled, (state, { payload }) => {
+//         state.accessToken = payload.accessToken;
+//         state.refreshToken = payload.refreshToken;
+//         state.sid = payload.sid;
+//         state.isLoggedIn = true;
+//         state.error = null;
+//         state.sessionError = false;
+//       })
+//       .addCase(logoutThunk.fulfilled, state => {
+//         return (state = initialState);
+//       })
+//       .addCase(refreshThunk.pending, state => {
+//         state.sessionError = false;
+//         state.isRefreshing = true;
+//       })
+//       .addCase(refreshThunk.fulfilled, (state, { payload }) => {
+//         state.sessionError = false;
+//         state.accessToken = payload.accessToken;
+//         state.refreshToken = payload.refreshToken;
+//         state.sid = payload.sid;
+//         state.isLoggedIn = true;
+//         state.isRefreshing = false;
+//       })
+//       .addCase(refreshThunk.rejected, state => {
+//         state.sessionError = true;
+//         state.isRefreshing = false;
+//       })
+//       .addMatcher(
+//         isAnyOf(registerThunk.pending, loginThunk.pending, logoutThunk.pending),
+//         (state, { payload }) => {
+//           state.sessionError = false;
+//           state.isLoading = true;
+//           state.error = null;
+//         }
+//       )
+//       .addMatcher(
+//         isAnyOf(
+//           registerThunk.rejected,
+//           loginThunk.rejected,
+//           logoutThunk.rejected
+//         ),
+//         (state, { payload }) => {
+//           state.sessionError = false;
+//           state.isLoading = false;
+//           state.error = payload.error;
+//         }
+//       ),
+// });
+
+// export const authReducer = authSlice.reducer;
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,77 +1,151 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { getCurrentUserThunk } from '../user/operations';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
-export const expenseApi = axios.create({
-  baseURL: 'https://expense-tracker.b.goit.study/api/',
-});
+axios.defaults.baseURL = "https://goit-expense-tracker.b.goit.study/api/";
+// axios.defaults.baseURL = 'https://goit-task-manager.herokuapp.com/';
 
-export const setToken = token => {
-  expenseApi.defaults.headers.common.Authorization = `Bearer ${token}`;
-};
-const clearToken = () => {
-  expenseApi.defaults.headers.common.Authorization = '';
+// Utility to add JWT
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
-export const registerThunk = createAsyncThunk(
-  'register',
-  async ({ name, email, password }, thunkApi) => {
+// Utility to remove JWT
+const clearAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = "";
+};
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ name, email, password }, thunkAPI) => {
     try {
-      const { data } = await expenseApi.post('auth/register', {
+      const response = await axios.post("/users/signup", {
         name,
         email,
         password,
       });
-      await thunkApi.dispatch(loginThunk({ email, password }));
-      return data;
+      setAuthHeader(response.data.token);
+      return response.data;
+      // console.log('response data', response.data);
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
-
-export const loginThunk = createAsyncThunk(
-  'login',
-  async (credential, thunkApi) => {
+export const logIn = createAsyncThunk(
+  "auth/logIn",
+  async ({ email, password }, thunkAPI) => {
     try {
-      const { data } = await expenseApi.post('auth/login', credential);
-      setToken(data.accessToken);
-      return data;
+      const response = await axios.post("/users/login", { email, password });
+      setAuthHeader(response.data.token);
+      return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
-
-export const logoutThunk = createAsyncThunk('logout', async (_, thunkApi) => {
+export const logOut = createAsyncThunk("auth/logout", async (_, thunkAPI) => {
   try {
-    await expenseApi.get('auth/logout');
-    clearToken();
+    await axios.post("/users/logout");
+    clearAuthHeader();
   } catch (error) {
-    return thunkApi.rejectWithValue(error.message);
+    return thunkAPI.rejectWithValue(error.message);
   }
 });
+export const refreshUser = createAsyncThunk(
+  "auth/refreshUser",
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state?.auth?.token;
 
-export const refreshThunk = createAsyncThunk(
-  'auth/refresh',
-  async (_, thunkApi) => {
-    const refreshData = {
-      refreshToken: thunkApi.getState().auth.refreshToken,
-      sid: thunkApi.getState().auth.sid,
-    };
-    if (!refreshData.refreshToken) {
-      return thunkApi.rejectWithValue('Token is not exist');
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue("Unable to fetch user");
     }
+
     try {
-      setToken(refreshData.refreshToken);
-      const { data } = await expenseApi.post('auth/refresh', {
-        sid: refreshData.sid,
-      });
-      setToken(data.accessToken);
-      thunkApi.dispatch(getCurrentUserThunk());
-      return data;
+      setAuthHeader(persistedToken);
+      const response = await axios.get("/users/current");
+      return response.data;
     } catch (error) {
-      return thunkApi.rejectWithValue(error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
+// import { createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
+// import { getCurrentUserThunk } from "../user/operations";
+
+// export const expenseApi = axios.create({
+//   baseURL: "https://goit-expense-tracker.b.goit.study/api/",
+// });
+
+// export const setToken = (token) => {
+//   expenseApi.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+// const clearToken = () => {
+//   expenseApi.defaults.headers.common.Authorization = "";
+// };
+
+// export const registerThunk = createAsyncThunk(
+//   "/register",
+//   async ({ name, email, password }, thunkApi) => {
+//     try {
+//       const { data } = await expenseApi.post("/auth/register", {
+//         name,
+//         email,
+//         password,
+//       });
+//       await thunkApi.dispatch(loginThunk({ email, password }));
+//       return data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const loginThunk = createAsyncThunk(
+//   "login",
+//   async (credential, thunkApi) => {
+//     try {
+//       const { data } = await expenseApi.post("auth/login", credential);
+//       setToken(data.accessToken);
+//       return data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// export const logoutThunk = createAsyncThunk("logout", async (_, thunkApi) => {
+//   try {
+//     await expenseApi.get("auth/logout");
+//     clearToken();
+//   } catch (error) {
+//     return thunkApi.rejectWithValue(error.message);
+//   }
+// });
+
+// export const refreshThunk = createAsyncThunk(
+//   "auth/refresh",
+//   async (_, thunkApi) => {
+//     const refreshData = {
+//       refreshToken: thunkApi.getState().auth.refreshToken,
+//       sid: thunkApi.getState().auth.sid,
+//     };
+//     if (!refreshData.refreshToken) {
+//       return thunkApi.rejectWithValue("Token is not exist");
+//     }
+//     try {
+//       setToken(refreshData.refreshToken);
+//       const { data } = await expenseApi.post("auth/refresh", {
+//         sid: refreshData.sid,
+//       });
+//       setToken(data.accessToken);
+//       thunkApi.dispatch(getCurrentUserThunk());
+//       return data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error.message);
+//     }
+//   }
+// );
+
+// https://goit-expense-tracker.b.goit.study/api/
