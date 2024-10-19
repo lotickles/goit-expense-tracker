ad2364ca98a32cb70cb4346670cbc1198cb689a1
lotickles <148078790+lotickles@users.noreply.github.com>
2024-10-19 07:41:13 +0000

    Deploying to gh-pages from @ lotickles/goit-expense-tracker@66e5c6c1da9f7b7b96549d6de00c73d47cb717d0 🚀

---
+++ static/js/172.5f1483a9.chunk.js
@@ -0,0 +1,58 @@
+"use strict";(self.webpackChunkgoit_expense_tracker=self.webpackChunkgoit_expense_tracker||[]).push([[172],{5172:(e,r,s)=>{s.r(r),s.d(r,{default:()=>k});var n=s(9456),a=s(9367),i=s(3054),o=s(3078),t=s(5043),d=s(4858),l=s(5475),p=s(2145),x=s(5058),c=s(6751),h=s(8777),m=s(1037),g=s(1529);g.Ay.form`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`,g.Ay.input`
+  padding: 10px;
+  margin-bottom: 10px;
+  border-radius: 6px;
+  border: none;
+  outline: 0;
+  background-color: #fff;
+  box-shadow: 0 1px 6px 0 #20212447;
+`,g.Ay.button`
+  padding: 10px 20px;
+  margin-top: 5px;
+  font-size: 14px;
+  border-radius: 6px;
+  border: 1px solid;
+  :hover {
+    background-color: #e0e0e0;
+  }
+`,g.Ay.p`
+  /* padding: 10px 20px; */
+  margin-bottom: 15px;
+`,g.Ay.span`
+  color: rgba(250, 250, 250, 0.4);
+`,g.Ay.button`
+  display: flex;
+  padding: 14px 44px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 40px;
+  border: 1px solid rgba(250, 250, 250, 0.4);
+  color: #fafafa;
+  font-family: Suisse Intl;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -0.32px;
+  text-decoration: none;
+  background: #0c0d0d;
+  &:hover {
+    border-radius: 40px;
+    border: 1px solid #0ef387;
+  }
+`;const u=g.Ay.div`
+  margin-top: 122px;
+  @media (min-width: 768px) and (max-width: 1439.8px) {
+    margin-top: 112px;
+  }
+
+  @media (min-width: 1440px) {
+    margin-top: 112px;
+  }
+`;var j=s(579);const f=()=>{const e=(0,n.wA)(),{handleSubmit:r,register:s,setValue:a,formState:{errors:i}}=(0,d.mN)(),[o,g]=(0,t.useState)(!1),[f,w]=(0,t.useState)("");return(0,j.jsx)("form",{onSubmit:r((r=>{e((0,x.eB)(r)).unwrap().catch((e=>{p.oR.error("Invalid email or password")}))})),children:(0,j.jsxs)(c.Uq,{children:[(0,j.jsxs)("div",{children:[(0,j.jsx)(c.sQ,{defaultValue:"",...s("email",{required:"Please provide your email",minLength:{value:6,message:"Ensure your email is at least 6 characters long"}}),type:"email",placeholder:"Email",autoComplete:"new-email"}),i.email&&(0,j.jsx)(c._,{children:i.email.message})]}),(0,j.jsxs)(c.qm,{children:[(0,j.jsxs)(c.Nj,{children:[(0,j.jsx)(c.rh,{...s("password",{required:"Enter a valid Password",minLength:{value:6}}),type:o?"text":"password",placeholder:"Password",autoComplete:"new-password",onChange:e=>{const r=e.target.value;w(r),a("password",r)},className:""+(0===f.length?"empty":f.length>=6&&!i.password?"valid":"invalid")}),(0,j.jsx)(c.s$,{onClick:()=>{g(!o)},type:"button",children:o?(0,j.jsx)(m.h,{}):(0,j.jsx)(h.h,{})})]}),i.password&&0===f.length&&(0,j.jsx)(c._,{children:i.password.message}),f.length>0&&(0,j.jsx)(c.Z3,{$isGood:f.length>=6,children:f.length>=6?"Good password":"Short password"})]}),(0,j.jsx)(u,{children:(0,j.jsx)(c.C_,{children:"Sign In"})}),(0,j.jsxs)(c.N1,{children:[(0,j.jsx)("p",{children:"New here?"}),(0,j.jsxs)(l.N_,{to:"/register",children:[(0,j.jsx)(c.L9,{children:"Create an account"})," "]})]})]})})};var w=s(7481),b=s(2501),y=s(1961),v=s(3216),S=s(4513);function k(){const e=(0,n.d4)(o.Ep),{name:r}=(0,n.d4)(S.mB);return e?(p.oR.success(`Welcome, ${r}`),(0,j.jsx)(v.C5,{to:"/transactions/expenses"})):(0,j.jsx)(a.vd,{children:(0,j.jsxs)("div",{children:[(0,j.jsx)(a.mg,{children:(0,j.jsx)("title",{children:"Sign in"})}),(0,j.jsx)(w.Sd,{children:(0,j.jsxs)(b.Tl,{children:[(0,j.jsxs)(b.DR,{children:[(0,j.jsx)(b.j8,{children:!e&&(0,j.jsx)(i.W,{})}),(0,j.jsx)(c.el,{src:y,alt:"Main "})]}),(0,j.jsxs)(b.$G,{children:[(0,j.jsx)(w.H2,{children:"Sign In"}),(0,j.jsx)(b.NT,{children:"Step into a world of hassle-free expense management! Your journey towards financial mastery begins here."}),(0,j.jsx)(f,{})]})]})})]})})}}}]);
+//# sourceMappingURL=172.5f1483a9.chunk.js.map
---
+++ static/js/569.267fbb22.chunk.js
@@ -0,0 +1,24 @@
+"use strict";(self.webpackChunkgoit_expense_tracker=self.webpackChunkgoit_expense_tracker||[]).push([[569],{2636:(t,e,r)=>{r.r(e),r.d(e,{default:()=>p});r(5043);const a=r.p+"static/media/404-page-not-found.9d370afd7b49866c0dd7.png";var o=r(1529);const i=o.Ay.div`
+  margin-top: 50px;
+  margin-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  padding: 50px;
+  background-color: transparent;
+  border-radius: 6px;
+  border: 1px solid white;
+  box-shadow: 0px 2px 4px 2px gray;
+  h1 {
+    font-size: 48px;
+    text-align: center;
+    margin-left: auto;
+    margin-right: auto;
+    color: white;
+  }
+`,n=o.Ay.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`;var d=r(579);const p=()=>(0,d.jsxs)(i,{children:[(0,d.jsx)("h1",{children:"Not found! 404"}),(0,d.jsx)(n,{src:a,alt:"404"})]})}}]);
+//# sourceMappingURL=569.267fbb22.chunk.js.map
